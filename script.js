2380146bbb0e7a7aa909dcfec23ad1eb2af1c897
ParfaitEGT <parfaitesaie@gmail.com>
2025-05-11 14:32:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Le site est chargé !");
-
+    
     fetch('content/actualites.json')
         .then(response => response.json())
         .then(data => {
             const actuContainer = document.getElementById("actu-liste");
-
-            // Vérifie que la div existe avant d'ajouter des éléments
             if (actuContainer) {
+                actuContainer.innerHTML = ""; // Efface le contenu avant d'ajouter des données
                 data.forEach(actu => {
                     const actuElement = document.createElement("div");
                     actuElement.innerHTML = `<h3>${actu.titre}</h3><p>${actu.contenu} (${actu.date})</p>`;
